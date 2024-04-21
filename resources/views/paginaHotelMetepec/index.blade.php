e8aad6c916ca22f95a3c7bc7b37f98c82f50f548
reyesArturo <programador_08@gob-ti.com>
2024-04-20 20:06:22 -0600

    modificacion de servicios

--- resources/views/paginaHotelMetepec/index.blade.php
+++ resources/views/paginaHotelMetepec/index.blade.php
@@ -1,175 +1,167 @@
 @extends('paginaHotelMetepec.layout.app')
 
 @section('contenido')
-<div class="banner__area" data-background="{{ asset('assets/img/banner-1.jpg') }}">
-	<div class="container">
-	  <div class="row">
-		<div class="col-xl-12">
-		  <div class="banner__area-title">
-			<h1>Hotel Metepec Plaza</h1>
-			<div class="banner__area-title-video">
-			  <div class="video__play">
-				<a class="video-popup" href="https://www.youtube.com/watch?v=0WC-tD-njcA"><i class="fas fa-play"></i></a>
-			  </div>
-			</div>
-		  </div>
-		</div>
-	  </div>
-  
-	</div>
-  </div>
-    	<!-- Video Area Start -->
-	<div class="video__area" data-background="assets/img/video.jpg">
-		<div class="container">
-			<div class="row align-items-center">
-				<div class="col-xxl-12 col-xl-12 col-lg-8">
-					<div class="video__area-title" style="text-align: center;">
-						<!-- <h2>Book hotel rooms, get deals & book flights online.</h2> -->
-						<h2 style="padding: 18px;">NUESTRAS HABITACIONES</h2>
-						<span style="text-align: center;color:#FFFFFF;font-size:22px;padding-top:10px;">Nuestras habitaciones ofrecen una experiencia de confort y descanso, con un diseño moderno y elegante. Contamos con habitaciones individuales, dobles y suites</span>
-					</div>
-				</div>
-			
-			</div>
-		</div>
-	</div>
-	<!-- Video Area End -->
-    	<!-- Services Area Start -->
-	<div class="services__area section-padding">
-		<div class="container">
-            <h2 style="text-align: center;padding-bottom: 50px;">Nuestros Servicios</h2>
-			<div class="row">
-				<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 lg-mb-30">
-					<div class="services__area-item">
-						<div class="services__area-item-icon">
-							<img src="assets/img/icon/cleaning.png" alt="">
-						</div>
-						<div class="services__area-item-content">
-							<h5><a href="#">Limpieza de la habitación</a></h5>
-							<p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 sm-mb-30">
-					<div class="services__area-item">
-						<div class="services__area-item-icon">
-							<img src="assets/img/icon/wifi.png" alt="">
-						</div>
-						<div class="services__area-item-content">
-							<h5><a href="#">Wifi</a></h5>
-							<p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-					<div class="services__area-item">
-						<div class="services__area-item-icon">
-							<img src="assets/img/icon/location.png" alt="">
-						</div>
-						<div class="services__area-item-content">
-							<h5><a href="#">Estacionamiento</a></h5>
-							<p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
-						</div>
-					</div>
-				</div>
-				
-			</div>
-			
-		</div>
-	</div>
-	<!-- Services Area End -->
-	    	<!-- Services Area Start -->
-			<div class="services__area ">
-				<div class="container">
-					{{-- <h2 style="text-align: center;padding-bottom: 50px;">Nuestros Servicios</h2> --}}
-					<div class="row">
-						<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 lg-mb-30">
-							<div class="services__area-item">
-								<div class="services__area-item-icon">
-									<img src="assets/img/icon/cleaning.png" alt="">
-								</div>
-								<div class="services__area-item-content">
-									<h5><a href="#">Limpieza de la habitación</a></h5>
-									<p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 sm-mb-30">
-							<div class="services__area-item">
-								<div class="services__area-item-icon">
-									<img src="assets/img/icon/wifi.png" alt="">
-								</div>
-								<div class="services__area-item-content">
-									<h5><a href="#">Wifi</a></h5>
-									<p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
-								</div>
-							</div>
-						</div>
-						<div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
-							<div class="services__area-item">
-								<div class="services__area-item-icon">
-									<img src="assets/img/icon/location.png" alt="">
-								</div>
-								<div class="services__area-item-content">
-									<h5><a href="#">Estacionamiento</a></h5>
-									<p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
-								</div>
-							</div>
-						</div>
-						
-					</div>
-					
-				</div>
-			</div>
-     	<!-- Accommodations Area Start -->
-         <div class="accommodations__area section-padding">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-5 col-lg-6 lg-mb-30">
-                        <div class="accommodations__area-title">
-                            <!-- <span class="subtitle__one">Accommodations</span> -->
-                            <h2>SOBRE NOSOTROS</h2>
-                            <p>¡Bienvenido a nuestro Hotel Metepec Plaza donde la comodidad y el estilo se fusionan para brindarle una experiencia inolvidable!</p>
-                            <a class="theme-btn" href="nosotros.html">Leer mas<i class="fal fa-long-arrow-right"></i></a>
+    <div class="banner__area" data-background="{{ asset('assets/img/banner-1.jpg') }}">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="banner__area-title">
+                        <h1>Hotel Metepec Plaza</h1>
+                        <div class="banner__area-title-video">
+                            <div class="video__play">
+                                <a class="video-popup" href="#"><i class="fas fa-play"></i></a>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-xl-7 col-lg-6">
-                        <div class="accommodations__area-right">
-                            <div class="accommodations__area-right-image">
-                                <img src="assets/img/hotel/hotel-1.jpg" alt="">
-                                <div class="accommodations__area-right-image-two">
-                                    <img src="assets/img/hotel/hotel-2.jpg" alt="">
-                                </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+		<div class="container-fluid mt-5">
+				<div class="video__area" data-background="assets/img/video.jpg">
+					<div class="row align-items-center">
+							<div class="col-xl-12 col-xl-12 col-lg-8">
+									<div class="video__area-title" style="text-align: center;">
+											<h2 style="padding: 18px;">NUESTRAS HABITACIONES</h2>
+											<span style="text-align: center;color:#FFFFFF;font-size:22px;padding-top:10px;">Nuestras
+													habitaciones ofrecen una experiencia de confort y descanso, con un diseño moderno y elegante.
+													Contamos con habitaciones individuales, dobles y suites</span>
+									</div>
+							</div>
+
+					</div>
+			</div>
+	</div>
+    <!-- Services Area Start -->
+    <div class="services__area section-padding ">
+        <div class="container">
+            <h2 class="text-center pb-5">Nuestros Servicios</h2>
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 my-4">
+                    <div class="services__area-item">
+                        <div class="services__area-item-icon">
+                            <img src="assets/img/icon/cleaning.png" alt="">
+                        </div>
+                        <div class="services__area-item-content">
+                            <h5><a href="#">Limpieza de la habitación</a></h5>
+                            <p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 my-4">
+                    <div class="services__area-item">
+                        <div class="services__area-item-icon">
+                            <img src="assets/img/icon/wifi.png" alt="">
+                        </div>
+                        <div class="services__area-item-content">
+                            <h5><a href="#">Wifi</a></h5>
+                            <p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 my-4">
+                    <div class="services__area-item">
+                        <div class="services__area-item-icon">
+                            <img src="assets/img/icon/location.png" alt="">
+                        </div>
+                        <div class="services__area-item-content">
+                            <h5><a href="#">Estacionamiento</a></h5>
+                            <p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 my-4">
+                    <div class="services__area-item">
+                        <div class="services__area-item-icon">
+                            <img src="assets/img/icon/location.png" alt="">
+                        </div>
+                        <div class="services__area-item-content">
+                            <h5><a href="#">Estacionamiento</a></h5>
+                            <p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 my-4">
+                    <div class="services__area-item">
+                        <div class="services__area-item-icon">
+                            <img src="assets/img/icon/location.png" alt="">
+                        </div>
+                        <div class="services__area-item-content">
+                            <h5><a href="#">Estacionamiento</a></h5>
+                            <p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 my-4">
+                    <div class="services__area-item">
+                        <div class="services__area-item-icon">
+                            <img src="assets/img/icon/location.png" alt="">
+                        </div>
+                        <div class="services__area-item-content">
+                            <h5><a href="#">Estacionamiento</a></h5>
+                            <p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+		
+    <!-- Services Area End -->
+    <!-- Services Area Start -->
+
+    <!-- Accommodations Area Start -->
+    <div class="accommodations__area section-padding">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-5 col-lg-6 lg-mb-30">
+                    <div class="accommodations__area-title">
+                        <!-- <span class="subtitle__one">Accommodations</span> -->
+                        <h2>SOBRE NOSOTROS</h2>
+                        <p>¡Bienvenido a nuestro Hotel Metepec Plaza donde la comodidad y el estilo se fusionan para
+                            brindarle una experiencia inolvidable!</p>
+                        <a class="theme-btn" href="nosotros.html">Leer mas<i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-xl-7 col-lg-6">
+                    <div class="accommodations__area-right">
+                        <div class="accommodations__area-right-image">
+                            <img src="assets/img/hotel/hotel-1.jpg" alt="">
+                            <div class="accommodations__area-right-image-two">
+                                <img src="assets/img/hotel/hotel-2.jpg" alt="">
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Accommodations Area End -->
-  	<!-- Feature Area Start -->
-	<div class="feature__area">
-		<div class="container">
-			<div class="row align-items-center bg-left mb-60">
-				<div class="col-xl-6 col-lg-6">
-					<div class="feature__area-image">
-						<img class="img__full" src="assets/img/features/feature-1.jpg" alt="">
-					</div>
-				</div>
-				<div class="col-xl-6 col-lg-6">
-					<div class="feature__area-left">
-						<div class="feature__area-left-title">
-							{{-- <span class="subtitle__one"></span> --}}
-							<h2>Restaurante Candelabro</h2>
-							<p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
-							<a class="theme-border-btn" href="services-details.html">Leer mas<i class="fal fa-long-arrow-right"></i></a>
-						</div>
-					</div>
-				</div>
-			</div>
+    </div>
+    <!-- Accommodations Area End -->
+    <!-- Feature Area Start -->
+    <div class="feature__area">
+        <div class="container">
+            <div class="row align-items-center bg-left mb-60">
+                <div class="col-xl-6 col-lg-6">
+                    <div class="feature__area-image">
+                        <img class="img__full" src="assets/img/features/feature-1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6">
+                    <div class="feature__area-left">
+                        <div class="feature__area-left-title">
+                            {{-- <span class="subtitle__one"></span> --}}
+                            <h2>Restaurante Candelabro</h2>
+                            <p>Breve descripcion sdfks sdfsidof sdfjsuidf jsdfusd jsdfhns uisbnfij</p>
+                            <a class="theme-border-btn" href="services-details.html">Leer mas<i
+                                    class="fal fa-long-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-		</div>
-	</div>
-	<!-- Feature Area End -->
-
+        </div>
+    </div>
+    <!-- Feature Area End -->
 @endsection
