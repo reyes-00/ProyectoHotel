6c2d27e2ca6adc467ab7b060d156ad5dbeb44db7
reyesArturo <arturo.web.gobti@gmail.com>
2024-04-24 15:38:28 -0600

    header

--- resources/views/paginaHotelMetepec/partials/header.blade.php
+++ resources/views/paginaHotelMetepec/partials/header.blade.php
@@ -1,43 +1,57 @@
-
-<!-- Header Area Start -->
+	<!-- Header Area Start -->
 	<div class="header__sticky">
-		<div class="header__area p-0">
+		<div class="header__area">
 			<div class="container custom__container">
 				<div class="header__area-menubar">
 					<div class="header__area-menubar-left">
 						<div class="header__area-menubar-left-logo">
-							<a href="{{ route('index') }}"><img src="{{ asset('assets/img/logo/logo.png') }}" alt="logo_hotel.png"></a>
+							<a href="index.html"><img src="assets/img/logo.png" alt=""></a>
 							<div class="responsive-menu"></div>
 						</div>
 					</div>
-					<div class="header__area-menubar-right" style="width: 66rem">
+					<div class="header__area-menubar-right">
 						<div class="header__area-menubar-right-menu menu-responsive">						
-								<ul id="mobilemenu" >
-									<li><a href="{{ route('index') }}">Inicio</a></li>
-									<li class="menu-item-has-children">
-											<a href="{{ route('galeria') }}">Habitaciones</a>
-											<ul class="sub-menu">
-													<li><a href="{{ route('habitacion_sencilla') }}">Habitacion Sencilla</a></li>
-													<li><a href="{{ route('habitacion_sencilla_plus') }}">Habitacion Sencilla Plus</a></li>
-													<li><a href="{{ route('habitacion_doble') }}">Habitacion Doble</a></li>
-													<li><a href="{{ route('habitacion_doble_plus') }}">Habitacion Doble Plus</a></li>
-													<li><a href="{{ route('suit') }}">Suite</a></li>
-													<li><a href="{{ route('habitaciones') }}">Ver Todas</a></li>
-											</ul>
-									</li>
-									<li><a href="{{ route('galeria') }}">Instalaciones</a></li>
-									<li><a href="{{ route('contacto') }}">Contacto</a></li>
-									<li><a href="{{ route('blog') }}">Blog</a></li>
-									<li class="header__area-menubar-right-box-btn">
-										<a class="theme-btn" href="#">Reservar<i class="fal fa-long-arrow-right"></i>
-										</a>
-									</li>
-								</ul>
-						
-						
+							<ul id="mobilemenu">
+								<li class="menu-item-has-children"><a href="#">Home</a>
+									<ul class="sub-menu">
+										<li><a href="index.html">Home 01</a></li>
+										<li><a href="index-2.html">Home 02</a></li>
+										<li><a href="index-3.html">Home 03</a></li>									
+									</ul>
+								</li>
+								<li class="menu-item-has-children"><a href="#">Pages</a>
+									<ul class="sub-menu">
+										<li><a href="about.html">About</a></li>
+										<li><a href="services-team.html">Team</a></li>
+										<li><a href="services-details.html">Services Details</a></li>
+										<li><a href="coming-soon.html">Coming Soon</a></li>
+									</ul>
+								</li>
+								<li class="menu-item-has-children"><a href="#">Room</a>
+									<ul class="sub-menu">
+										<li><a href="room-style.html">Room Style</a></li>
+										<li><a href="room-modern.html">Room Modern</a></li>
+										<li><a href="room-list.html">Room List</a></li>
+										<li><a href="room-details.html">Room Details</a></li>
+									</ul>
+								</li>
+								<li class="menu-item-has-children"><a href="#">Blog</a>
+									<ul class="sub-menu">
+										<li><a href="blog-grid.html">Blog Grid</a></li>
+										<li><a href="blog-standard.html">Blog Standard</a></li>
+										<li><a href="blog-details.html">Blog Details</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact</a></li>
+								<li><a href="galeria.html">Galeria</a></li>
+							</ul>
 						</div>
 					</div>
-
+					<div class="header__area-menubar-right-box">
+						<div class="header__area-menubar-right-box-btn">
+							<a class="theme-btn" href="contact.html">Book Now<i class="fal fa-long-arrow-right"></i></a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
