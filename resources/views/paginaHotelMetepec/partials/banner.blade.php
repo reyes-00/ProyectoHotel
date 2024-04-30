ec255197325bec014ee55d291b952d91eabbaae9
reyesArturo <arturo.web.gobti@gmail.com>
2024-04-30 09:37:28 -0600

    video
    video

--- resources/views/paginaHotelMetepec/partials/banner.blade.php
+++ resources/views/paginaHotelMetepec/partials/banner.blade.php
@@ -1,21 +1,40 @@
 <style>
   video{
-    width: 100%
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .capa{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #1c1c1d
+    opacity: 0.5;
+    mix-blend-mode: overlay;
   }
 </style>
 
-<div class="banner__area" data-background="{{ asset('assets/img/banner-1.jpg') }}"  style="border:1px solid red">
+<div class="banner__area" >
   <div class="container-fluid"  style="border:1px solid blue">
-    <div class="row"  style="border:1px solid red">
+    <div class="row" >
       <div class="col-xl-12">
         <div class="banner__area-title">
-          <h1>Hotel Metepec Plaza</h1>
-          <div class="banner__area-title-video" style="border:1px solid red">
-            <div class="video__play">
-              <a class="video-popup" href="https://www.youtube.com/watch?v=0WC-tD-njcA"><i class="fas fa-play"></i></a>
-            </div>
+          <h1 style="position: relative; z-index:2;">Hotel Metepec Plaza</h1>
+          <div class="banner__area-title-video">
+            {{-- <div class="video__play">
+              <a class="video-popup" href="#"><i class="fas fa-play"></i></a>
+            </div> --}}
           </div>
-          <video src="{{ asset('assets/video/videoBannerHome.mp4') }}" autoplay muted loop class="background"></video>
+          <video muted autoplay loop>
+            <source src="{{ asset('assets/video/videoBannerHome.mp4') }}" type="video/mp4">
+          </video>
+          <div class="capa"></div>
         </div>
       </div>
     </div>
