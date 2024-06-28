2ed9c27e67e73bad2c601629f325abd568774479
reyesArturo <boower-00@hotmail.com>
2024-06-28 10:35:25 -0600

    Se coloco dos puntos a telefonos

--- resources/views/paginaHotelMetepec/contacto.blade.php
+++ resources/views/paginaHotelMetepec/contacto.blade.php
@@ -18,7 +18,7 @@
 								<i class="fal fa-phone-alt"></i>
 							</div>
 							<div class="contact__area-info-item-content">
-								<span>Telefono</span>
+								<span>Telefono:</span>
 								<h6><a href="">+52 722 232 42 96(97,98)</a></h6>
 								<h6><a href="">+52 722 232 42 42</a></h6>
 							</div>
