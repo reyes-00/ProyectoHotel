59ad5864c7aa41dad5a9c4fd4ab70598415cf0f7
reyesArturo <arturo.web.gobti@gmail.com>
2024-06-28 14:31:32 -0600

    cambios
    cambios

--- resources/views/paginaHotelMetepec/partials/banner.blade.php
+++ resources/views/paginaHotelMetepec/partials/banner.blade.php
@@ -1,100 +1,81 @@
 <style>
-  video {
+  video{
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     object-fit: cover;
-    
-    -webkit-object-fit: cover;
-    -moz-object-fit: cover;
-    -ms-object-fit: cover;
-    -o-object-fit: cover;
   }
 
-  /* .capa {
+  .capa{
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    background: #1c1c1d;
+    background: #1c1c1d
     opacity: 0.5;
     mix-blend-mode: overlay;
-   
-    -webkit-mix-blend-mode: overlay;
-    -moz-mix-blend-mode: overlay;
-    -ms-mix-blend-mode: overlay;
-    -o-mix-blend-mode: overlay;
-  } */
-
-  /* .video-container {
-    position: fixed;
-    top: 10%;
-    left: 10%;
-    width: 80%;
-    height: 80%;
-    background: black;
-    z-index: 9999;
-    overflow: hidden; 
-  } */
-
-  #close-btn {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    background: white;
-    color: black;
-    border: none;
-    cursor: pointer;
-    padding: 10px; /* Aumenta el tamaño para mejor accesibilidad */
-    font-size: 16px; /* Aumenta el tamaño del texto para mejor accesibilidad */
   }
+  .video-container {
+  position: fixed;
+  top: 10%;
+  left: 10%;
+  width: 80%;
+  height: 80%;
+  background: black;
+  z-index: 9999;
+}
 
-  /* Estilos adicionales para mejor compatibilidad y accesibilidad */
-  #close-btn:focus {
-    outline: 2px solid #000;
-  }
+#close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: white;
+  color: black;
+  border: none;
+  cursor: pointer;
+}
 </style>
 
-
-<div class="banner__area">
-  <div class="container-fluid">
-    <div class="row">
+<div class="" >
+  <div class="container">
+    <div class="">
       <div class="col-xl-12" style="display: flex; justify-content:center;">
         <div class="banner__area-title">
           {{-- <h1 style="position: relative; z-index:2;">{{ __('Hotel Metepec Plaza') }}</h1> --}}
           <img src="{{ asset('assets/img/logos/logo_B.png') }}" width="300px" alt="logo" style="position: relative; z-index:2;">
-          <div class="video__play" style="position: relative; z-index:2;">
-            <a class="video-popup d-block mx-auto" href="#"><i class="fas fa-play "></i></a>
-          </div>
+            <div class="video__play" style="position: relative; z-index:2;">
+              <a class="video-popup d-block mx-auto" href="#"><i class="fas fa-play "></i></a>
+            </div>
           <video muted autoplay loop>
             <source src="{{ asset('assets/video/videoBannerHome.mp4') }}" type="video/mp4">
-            <source src="{{ asset('assets/video/videoBannerHome.mov') }}" type="video/quicktime">
-            <source src="{{ asset('assets/video/videoBannerHome.webm') }}" type="video/webm">
-            Tu navegador no soporta el elemento de video.
-          </video>          
+          </video>
           <div class="capa"></div>
         </div>
       </div>
     </div>
+
   </div>
+
 </div>
 
 <script>
   document.querySelector('.video-popup').addEventListener('click', function(e) {
-    e.preventDefault();
-    var video = document.querySelector('video');
+  e.preventDefault();
+  var video = document.querySelector('video');
   
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-    } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) {
-      video.msRequestFullscreen();
-    }
-  });
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    video.msRequestFullscreen();
+  }
+});
+
+
 </script>
